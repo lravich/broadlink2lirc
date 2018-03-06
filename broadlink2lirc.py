64b11baeaf686f7d74ae36179f073795bd3dd1f2
Leonid Ravich <lravich@gmail.com>
2018-03-06 22:18:44 +0200

    adding broadlink record examples

--- broadlink2lirc.py
+++ broadlink2lirc.py
@@ -1,7 +1,12 @@
 import binascii
 import struct
 import os
+
+
 class lircPkt:
+	def __iter__(self):
+		return iter(self.lirc_pkt)
+		
 	def __str__(self):
 		out = ""
 		i = 0
@@ -13,52 +18,58 @@ class lircPkt:
 		return out
 	
 		return self.lirc_pkt.__str__()
-	def __init__(self, data, length):
-		self.lirc_pkt = []
-		i = 0
-		while(1):
-			if (int(data[i]) == 0):
-				self.lirc_pkt.append(struct.unpack(">H", data[i+1:i+3])[0])
-				i = i + 3
-			else:
-				self.lirc_pkt.append(int(data[i]))
-				i = i + 1
-			if (i == length):
-				break
-class broadPkt:
+	def __init__(self, pulse_list):
+		self.lirc_pkt = pulse_list
+
+class broadPkt(lircPkt):
+	IR_CODE = 0x26
+	IR="IR"
+	RF="RF"
 	def __str__(self):
-		return "Type: {} Repeat: {} length {} \n {}".format(self.type, self.repeat, self.len, self.lirc)
-	def decode_ir(self, ir_data):
-		self.lirc = lircPkt(ir_data, self.len)
-	def decode(self, data):
+		return "Type: {} Repeat: {} length {} \n {}".format(self.type, self.repeat, self.len, lircPkt.__str__(self))
+
+	def __init__(self, pkt_type, repeat, length, pulse_list):
+		self.type = pkt_type
+		self.repeat = repeat
+		self.len = length
+		lircPkt.__init__(self, pulse_list)
+	
+	@classmethod
+	def fromBase64(cls, data):
 		bytes_raw = bytearray(data.decode("base64"))
-		if (int(bytes_raw[0]) == 0x26):
-			self.type = "IR"
+		if (int(bytes_raw[0]) == broadPkt.IR_CODE):
+			pkt_type = broadPkt.IR
 		else:
-			self.type = "RF"
-		self.repeat = int(bytes_raw[1])
-		self.len = struct.unpack("<H", bytes_raw[2:4])[0]
-		if (self.type == "IR"):
-			self.decode_ir(bytes_raw[4:])
+			pkt_type = broadPkt.RF
 
-	def __init__(self, data):
-		if (data):
-			self.decode(data)
-
-		
-def main():
-	path ="/home/leonid/ac_samples"
-	out_file = open("ac_dec",'w')
+		repeat = int(bytes_raw[1])
+		length = struct.unpack("<H", bytes_raw[2:4])[0]
+		if (pkt_type == broadPkt.IR):
+			lirc_data = bytes_raw[4:]
+			pulse_list = []
+			i = 0
+			while(1):
+				if (int(lirc_data[i]) == 0):
+					pulse_list.append(struct.unpack(">H", lirc_data[i+1:i+3])[0])
+					i = i + 3
+				else:
+					pulse_list.append(int(lirc_data[i]))
+					i = i + 1
+				if (i == length):
+					break
+			return cls(pkt_type, repeat, length, pulse_list)
+		else:
+			raise NotImplemented
+			
+def example():
+	path ="ac_samples"
 	for filename in os.listdir(path):
 		abs_filename = os.path.join(path, filename)
 		fd = open(abs_filename)
 		data_base64 = fd.read()
-		bp = broadPkt(data_base64)
+		bp = broadPkt.fromBase64(data_base64)
 		print bp
 		fd.close()
-	#	out_file.write(filename + "->" + dec_data + "\n")
-
-	out_file.close()
 
 if __name__== "__main__":
-	main()
+	example()
